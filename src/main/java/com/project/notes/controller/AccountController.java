c661bcd1c98fc652e14a188c198d68eebf7b1d53
romain-cotoni <romain.cotoni@gmail.com>
2023-12-31 00:54:56 +0100

    set crossorigins without "*"

--- src/main/java/com/project/notes/controller/AccountController.java
+++ src/main/java/com/project/notes/controller/AccountController.java
@@ -19,7 +19,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"*","https://black-tree-097fbec03.4.azurestaticapps.net", "https://www.getpostman.com", "http://localhost:4200/"}, allowCredentials = "true", maxAge=3600)
+//@CrossOrigin(origins = {"*","https://black-tree-097fbec03.4.azurestaticapps.net", "https://www.getpostman.com", "http://localhost:4200/"}, allowCredentials = "true", maxAge=3600)
+@CrossOrigin(origins = {"https://black-tree-097fbec03.4.azurestaticapps.net"}, allowCredentials = "true", maxAge=3600)
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
