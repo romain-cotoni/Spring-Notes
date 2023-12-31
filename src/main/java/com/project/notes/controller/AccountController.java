c2c85640349fe0280171fe94f21e57028b4174e5
romain-cotoni <romain.cotoni@gmail.com>
2023-12-31 14:24:56 +0100

    test and search url for controller & websecurity config

--- src/main/java/com/project/notes/controller/AccountController.java
+++ src/main/java/com/project/notes/controller/AccountController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 //@CrossOrigin(origins = {"https://black-tree-097fbec03.4.azurestaticapps.net"}, allowCredentials = "true", maxAge=3600)
 //@CrossOrigin(origins = "*", allowedHeaders = "*")
-@CrossOrigin(origins = {"https://black-tree-097fbec03.4.azurestaticapps.net/", "https://www.getpostman.com", "http://localhost:4200/"}, allowCredentials = "true", maxAge=3600)
+@CrossOrigin(origins = {"https://black-tree-097fbec03.4.azurestaticapps.net", "https://www.getpostman.com", "http://localhost:4200/"}, allowCredentials = "true", maxAge=3600)
 @RestController
 @RequestMapping("api/account")
 public class AccountController {
