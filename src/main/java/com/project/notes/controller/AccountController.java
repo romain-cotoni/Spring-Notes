98b72c2a8b783d3c617c9d14fe7dbaa2d311a933
romain-cotoni <romain.cotoni@gmail.com>
2023-12-31 14:09:19 +0100

    test and search url for controller & websecurity config

--- src/main/java/com/project/notes/controller/AccountController.java
+++ src/main/java/com/project/notes/controller/AccountController.java
@@ -23,7 +23,7 @@ import java.util.List;
 //@CrossOrigin(origins = {"https://black-tree-097fbec03.4.azurestaticapps.net"}, allowCredentials = "true", maxAge=3600)
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("https://spring-notes-spring-notes.azuremicroservices.io/api/account")
+@RequestMapping("api/account")
 public class AccountController {
     private final AccountService accountService;
     private final AuthenticationManager authenticationManager;
